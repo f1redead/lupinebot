b44c46d909594714d6cc60cf3aa9f149dbba9f10
f1redead <dqk-r@ya.ru>
2014-05-09 15:35:20 +0400

    Plugin improves and fixes

--- plugins/misc.py
+++ plugins/misc.py
@@ -1,10 +1,20 @@
 # *-* encoding: utf-8 *-*
 
 # This is example cBot plugin
-cmds = ['ping', 'test']
 
-def action(event, body, message_type):
-	if body[0] == "ping":
-		event.reply("O_o").send()
-	elif body[0] == "test":
-		event.reply("T_T").send()
+def ping(event, body, args, bot, message_type):
+	bot.reply(event, "pong!")
+
+def say(event, body, args, bot, message_type):
+	bot.reply(event, body[1], with_nickname = False)
+
+def join(event, body, args, bot, message_type):
+	if len(args) == 2:
+		conference, nickname = args
+		bot.join_muc(conference, nickname)
+
+metainfo = {
+	'ping': {'function': ping, 'descr': "Понг!", 'privlevel': 1, 'aliases': ['пинг'], 'need_prefix': False},
+	'say': {'function': say, 'descr': "Отправит сообщение от имени бота", 'privlevel': 100, 'aliases': ['сказать'], 'need_prefix': True},
+	'join': {'function': join, 'descr': "Зайти в комнату", 'privlevel': 100, 'aliases': ['зайти'], 'need_prefix': True}
+}
--- run.py
+++ run.py
@@ -14,10 +14,15 @@ import logging
 class XMPPBot(ClientXMPP):
 	def __init__(self):
 		self.start_handlers = {
-								'session_start': self.session_start,
-								'message': self.message_received,
-								'muc::{conference}::got_online': self.muc_online	
-							}
+			'session_start': self.session_start,
+			'message': self.message_received,
+			'muc::{conference}::got_online': self.muc_online	
+		}
+		self.counters = {
+			'total_commands': 0,
+			'total_plugins': 0
+		}
+		self.users = {}
 		self.xeps_to_load = ["xep_0092", "xep_0045"]
 		self.read_configs()
 
@@ -34,12 +39,14 @@ class XMPPBot(ClientXMPP):
 		log("My JID is {jid}".format(jid=colored(self.settings['auth']['jid'], "cyan")))
 
 		self.plugins = self.load_plugins()
-		log("Plugins list: " + ', '.join(self.plugins['commands']))
-		log(self.plugins['info'])
+
 		self.register_handlers()
 
 	def format_platform(self, platform):
-		return platform.format(python_version = self.python_version, os_name = sys.platform.capitalize())
+		return platform.format(
+			python_version = self.python_version, 
+			os_name = sys.platform.capitalize()
+		)
 
 	def read_configs(self):
 		self.settings = {}
@@ -54,49 +61,98 @@ class XMPPBot(ClientXMPP):
 						self.settings[section][item] = getpass.getpass("Enter password for {jid}: ".format(jid=self.settings['auth']['jid']))
 					elif value in ("0", "1"):
 						self.settings[section][item] = config_inst.getboolean(section, item)
-		self.muc_list = self.settings['muc']['list'].split(",")
+		self.muc_list = []
+		for muc in self.settings['muc']['list'].split(","):
+			conference, nickname = muc.split("/")
+			self.muc_list.append({'conference': conference, 'nickname': nickname})
+
+		self.have_prefix = self.settings['muc']['prefix'] != ""
+		if self.have_prefix: self.prefix = self.settings['muc']['prefix']
+
+		self.nicknames = [muc['nickname'] for muc in self.muc_list]
+
+	def join_muc(self, conference, nickname):
+		log("Joining to {conference} with nickname: {nickname}".format(
+				conference=colored(conference, "red"), 
+				nickname=colored(nickname, "cyan")
+		))
+		for handler, function in self.start_handlers.items():
+			if handler.startswith("muc"):
+				self.register_muc_handler(handler, function, conference)
+		self.plugin['xep_0045'].joinMUC(conference, nickname, wait=True)
+		self.users[conference] = {}
+
+	def register_muc_handler(self, handler, function, conference):
+		handler = handler.format(conference=conference)
+		log("Registering MUC handler: {handler}".format(
+			handler=handler.replace(conference, colored(conference, "red"))
+		))
+		self.add_event_handler(handler, function)		
 
 	def register_handlers(self):
 		for handler, function in self.start_handlers.items():
-			if handler.startswith("muc"):
-				for muc in self.muc_list:
-					handler = handler.format(conference=muc)
-					log("Registering MUC handler: {handler}".format(handler=handler.replace(muc, colored(muc, "red"))))
-					self.add_event_handler(handler, function)
-			else:
+			if not handler.startswith("muc"):
 				log("Registering handler: {handler}".format(handler=handler))
 				self.add_event_handler(handler, function)
 
 	def join_mucs(self):
 		for muc in self.muc_list:
-			self.plugin['xep_0045'].joinMUC(muc, self.settings['muc']['nickname'], wait=True)
+			self.join_muc(muc['conference'], muc['nickname'])
 
 	def session_start(self, event):
 		self.send_presence()
 		self.get_roster()
 		self.join_mucs()
 
+	def get_message_type(self, event):
+		return 'normal' if event['type'] in ('chat', 'normal') else 'groupchat'
+
+	def get_shortname(self, event):
+		# return str(event['from']).split("/")[1]
+		return event['mucnick']
+
+	def get_muc(self, event):
+		return str(event['from']).split("/")[0]
+
+	def get_command(self, command):
+		for cmd_name, aliases in self.plugins['commands'].items():
+			if command in aliases:
+				return cmd_name
+		return None
+
+	def reply(self, event, text, with_nickname = True):
+		message_type = self.get_message_type(event)
+
+		if message_type == "normal": text = text.capitalize()
+		elif message_type == "groupchat" and with_nickname: text = "{nickname}: ".format(nickname=self.get_shortname(event)) + text
+
+		event.reply(text).send()
+
 	def message_received(self, event):
-		body = event['body'].split(" ", 1)
-		message_type = 'normal' if event['type'] in ('chat', 'normal') else 'groupchat'
-		self.command_handler(event, body, message_type)
+		if self.have_prefix and event['body'].startswith(self.prefix): body = event['body'][len(self.prefix):].split(" ", 1)
+		else: body = event['body'].split(" ", 1)
 
+		if len(body) == 1: body = body + ['']
+
+		# Command handler
+		cmd = body[0]
+		args = body[1].split(" ")
+		if self.get_command(cmd) != None:
+			if self.have_prefix and event['body'].startswith(self.prefix) or not self.plugins['info'][cmd]['need_prefix']: 
+				if self.get_shortname(event) not in self.nicknames:
+					self.call_plugin(event, body, args, self, self.get_message_type(event))
 	def get_filepaths(self, directory):
-	    file_paths = []
+		file_paths = []
 
-	    for root, directories, files in os.walk(directory):
-	        for filename in files:
-	            filepath = os.path.join(root, filename)
-	            file_paths.append(filepath)
+		for root, directories, files in os.walk(directory):
+			for filename in files:
+				filepath = os.path.join(root, filename)
+				file_paths.append(filepath)
 
-	    return file_paths
-
-	def command_handler(self, event, body, message_type):
-		if body[0] in self.plugins['commands']:
-			self.call_plugin(event, body, message_type)
+		return file_paths
 
 	def load_plugins(self):
-		commands = []
+		commands = {}
 		info = {}
 		for filepath in self.get_filepaths("plugins"):
 			if filepath.endswith(".py"):
@@ -105,18 +161,48 @@ class XMPPBot(ClientXMPP):
 				if plugin_name != "__init__":
 					plugins_import = __import__(plugin_path)
 					plugin = getattr(plugins_import, plugin_name)
-					# Like so: {'ping': 'misc'}
-					for cmd in plugin.cmds:
-						info[cmd] = plugin_name
-					commands.extend(plugin.cmds)
+					for cmd_name, data in plugin.metainfo.items():
+						copy_of_data = data.copy()
+						copy_of_data.update({'category': plugin_name})
+						info[cmd_name] = copy_of_data 
+						
+						# Looks like so: {'ping': ['ping', 'пинг']}
+						commands[cmd_name] = [cmd_name] if len(data['aliases']) == 0 else [cmd_name] + data['aliases']
+
+						self.counters['total_commands'] += len(commands[cmd_name])
+						self.counters['total_plugins'] += 1
+
+						log("Loaded plugin: {category}:{cmd_name}. Commands: {commands}".format(
+							category = plugin_name,
+							cmd_name = cmd_name,
+							commands = ', '.join(commands[cmd_name]).replace(cmd_name, colored(cmd_name, "yellow"))
+						))
+
+		log("Total plugins loaded: %s" % colored(str(self.counters['total_plugins']), "magenta", attrs=['bold']))
+		log("Total commands: %s" % colored(str(self.counters['total_commands']), "magenta", attrs=['bold']))
 		return {'attrs': plugins_import, 'commands': commands, 'info': info}
 
-	def call_plugin(self, event, body, message_type):
-		plugin = getattr(self.plugins['attrs'], self.plugins['info'][body[0]])
-		plugin.action(event, body, message_type)
+	def call_plugin(self, event, body, args, bot, message_type):
+		cmd = self.get_command(body[0])
+		plugin = getattr(self.plugins['attrs'], self.plugins['info'][cmd]['category'])
+		self.plugins['info'][cmd]['function'](event, body, args, bot, message_type)
 
-	def muc_online(self, event):
-		pass
+	def presence_reply(self, presence, text, message_type = 'groupchat'):
+		self.send_message(mto=presence['from'].bare, mbody=text, mtype=message_type)
+
+	def muc_online(self, presence):
+		if presence['muc']['nick'] not in self.nicknames:
+			nick = presence['muc']['nick']
+			room = self.get_muc(presence)
+			affiliation = str(presence['muc']['affiliation'])
+
+			if affiliation == "owner": privlevel = 10
+			elif affiliation == "admin": privlevel = 5
+			else: privlevel = 1
+
+			self.users[room][nick] = {
+				'privlevel': privlevel
+			}
 		# You can do many cool things in this place, yes..
 
 if __name__ == "__main__":
